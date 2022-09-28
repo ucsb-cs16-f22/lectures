004cc5d457d9ce9222b6ea36eacf7029e172c89a
Diba Mirza <dimirza@cs.ucsb.edu>
2022-09-28 13:08:20 -0700

    lect-02 code

---
+++ lect02/game.cpp
@@ -0,0 +1,47 @@
+// game.cpp
+// Diba Mirza
+#include <iostream>
+#include <time.h>
+using namespace std;
+
+
+int generate_num(); // function declaration
+string check_match(int, int);
+
+int main(){
+ 	int guess, actual;	
+	string result;
+	// Read a number as input
+	cout << "Guess a number from 1 to 10: ";
+	cin >> guess;
+	// Generate a random number
+	srand(time(NULL));
+	actual = generate_num(); // function call
+	while(true){
+		// Check if the two numbers match and store the result 
+		result = check_match(guess, actual);
+		// Print the result
+		cout << result;
+		if( result ==  "Yay! You won!\n"){
+			break;
+		}
+		cin >> guess ;
+	}
+	cout << "Computer picked " << actual << endl;
+	return 0;
+}
+
+int generate_num(){
+	// function definition
+	return rand() % 10 + 1;
+}
+string check_match(int guess, int actual){
+	if (guess == actual){
+		return "Yay! You won!\n";
+	} else if(guess < actual){
+		return "Go higher\n";
+	} else {
+		return "Go lower\n";
+	}
+
+}
---
+++ lect02/hello.cpp
@@ -0,0 +1,21 @@
+// hello.cpp
+// Diba Mirza
+#include <iostream>
+using namespace std;
+
+int main(){
+	cout << "Hello World" << endl;
+	cout << "Bye bye World" << endl;
+	cout << "Bye bye World" << endl;
+	cout << "Bye bye World" << endl;
+	cout << "Bye bye World" << endl;
+	cout << "Bye bye World" << endl;
+	cout << "Bye bye World" << endl;
+	cout << "Bye bye World" << endl;
+	cout << "Bye bye World" << endl;
+	cout << "Bye bye World" << endl;
+	cout << "Bye bye World" << endl;
+	cout << "Bye bye World" << endl;
+	cout << "Bye bye World" << endl;
+	return 0;
+}
