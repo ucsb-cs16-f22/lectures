153b77eed8a3209c493d955794869a60d0a8a60a
dibamirza <dimirza@cs.ucsb.edu>
2022-10-17 12:38:27 -0700

    lect07

--- lect07/swap.cpp
+++ lect07/swap.cpp
@@ -2,16 +2,35 @@
 #include <iostream>
 using namespace std;
 
-void swap(int x, int y){
-    int tmp = x;
-    x = y;
-    y = tmp;
+// Precondition: x and y point to two integers
+// Postcondition: The values pointed to by x and y are swapped
+void swap(int* x, int* y){
+    cout << "x: " << x << " y: " << y << endl;   
+    int tmp = *x;
+    *x = *y;
+    *y = tmp;
 }
 
 int main(int argc, char const *argv[])
 {
-    int a = 30, b = 40;      
-    swap(a,  b);
+    int a = 30, b = 40;  
+    cout << "&a: " << &a << " &b: " << &b << endl; 
+    cout << "a = " << a << endl;  //A. 30 B. 70
+    int *q = nullptr, *w = nullptr;
+
+    //int* q = nullptr;
+    //int* w = nullptr
+
+    q = &a;
+    w = &b;
+    swap (&a, &b);
+    //swap(q,  w);
     cout << a << "  " << b << endl;
+    // int* q = nullptr;  
+    // int* p = &a;
+    // cout << "p = " << p << endl;
+    // cout << "&p = " << &p << endl;
+    // cout << "*p = " << *p << endl;
+    // *p = 70;
     return 0;
 }
